3fe3fbec22155fabf47f7a46b282c934b8c896fe
Cathal0317 <hlzh0610@gmail.com>
2026-02-16 15:28:50 +0000

    16/2

---
+++ Round2/Anagram.py
@@ -0,0 +1,26 @@
+# Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+# An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+# Example 1:
+
+# Input: s = "racecar", t = "carrace"
+
+# Output: true
+# Example 2:
+
+# Input: s = "jar", t = "jam"
+
+# Output: false
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ndicts = defaultdict(int)
+        ndictt = defaultdict(int)
+
+        for i in s:
+            ndicts[i] += 1
+        for j in t:
+            ndictt[j] += 1
+        
+        return ndictt == ndicts
