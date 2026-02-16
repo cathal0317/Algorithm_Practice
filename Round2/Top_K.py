e782c11df220f692ab8a178ed0adda249027a4a3
Cathal0317 <hlzh0610@gmail.com>
2026-02-16 16:28:32 +0000

    16/2

---
+++ Round2/Top_K.py
@@ -0,0 +1,32 @@
+# Given an integer array nums and an integer k, return the k most frequent elements within the array.
+
+# The test cases are generated such that the answer is always unique.
+
+# You may return the output in any order.
+
+# Example 1:
+
+# Input: nums = [1,2,2,3,3,3], k = 2
+
+# Output: [2,3]
+# Example 2:
+
+# Input: nums = [7,7], k = 1
+
+# Output: [7]
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        ndict = defaultdict(int)
+        for num in nums:
+            ndict[num] += 1
+        
+        heap = []
+        for a in ndict.keys():
+            heapq.heappush(heap, (a, ndict[a]))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(heap)[0])
+        return res
