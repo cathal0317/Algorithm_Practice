4909411c826bbd17d150a476a746c9d7b3050b31
Cathal0317 <hlzh0610@gmail.com>
2026-02-17 12:24:03 +0000

    17/2

---
+++ Round2/Longest_Substring.py
@@ -0,0 +1,30 @@
+# Given a string s, find the length of the longest substring without duplicate characters.
+
+# A substring is a contiguous sequence of characters within a string.
+
+# Example 1:
+
+# Input: s = "zxyzxyz"
+
+# Output: 3
+# Explanation: The string "xyz" is the longest without duplicate characters.
+
+# Example 2:
+
+# Input: s = "xxxx"
+
+# Output: 1
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        rset = set()
+        total = 0
+        l = 0
+
+        for r in range(len(s)):
+            while s[r] in rset:
+                rset.remove(s[l])
+                l+=1
+            rset.add(s[r])
+            total = max(total, r-l+1)
+        return total
