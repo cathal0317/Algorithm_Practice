2be5928d43780ac63dc3d2d40c8c5a9607627341
Cathal0317 <hlzh0610@gmail.com>
2026-03-11 16:29:06 +0000

    10/3

---
+++ Round2/Water_Container.py
@@ -0,0 +1,30 @@
+# You are given an integer array heights where heights[i] represents the height of the 
+# i
+# t
+# h
+# i 
+# th
+#   bar.
+
+# You may choose any two bars to form a container. Return the maximum amount of water a container can store.
+
+# Example 1:
+
+
+
+# Input: height = [1,7,2,5,4,7,3,6]
+
+# Output: 36
+
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        maxv = 0
+        l, r = 0, len(heights) - 1
+        while l < r:
+            area = (r-l) * min(heights[r],heights[l])
+            maxv = max(maxv, area)
+            if heights[l] < heights[r]:
+                l+=1
+            else:
+                r-=1
+        return maxv
