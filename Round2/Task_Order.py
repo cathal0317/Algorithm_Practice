af59acaaec798f562fbe4b557264867cfc8f8b26
Cathal0317 <hlzh0610@gmail.com>
2026-04-09 13:49:59 +0100

    8/4

---
+++ Round2/Task_Order.py
@@ -0,0 +1,33 @@
+# You are given an array of CPU tasks tasks, where tasks[i] is an uppercase english character from A to Z. You are also given an integer n.
+
+# Each CPU cycle allows the completion of a single task, and tasks may be completed in any order.
+
+# The only constraint is that identical tasks must be separated by at least n CPU cycles, to cooldown the CPU.
+
+# Return the minimum number of CPU cycles required to complete all tasks.
+
+# Example 1:
+
+# Input: tasks = ["X","X","Y","Y"], n = 2
+
+# Output: 5
+# Explanation: A possible sequence is: X -> Y -> idle -> X -> Y.
+
+# Example 2:
+
+# Input: tasks = ["A","A","A","B","C"], n = 3
+
+# Output: 9
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        ndict = defaultdict(int)
+        for char in tasks:
+            ndict[char] += 1
+        maxf = max(ndict.values())
+        count = sum(1 for v in ndict.values() if v == maxf)
+        time = (maxf - 1) * (n + 1) + count
+        return max(len(tasks), time)
+
+
+        
