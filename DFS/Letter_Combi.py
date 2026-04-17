8c71207533e6dc095801c15cb853e2042fbbf551
Cathal0317 <hlzh0610@gmail.com>
2026-04-17 09:45:44 +0100

    13/4

---
+++ DFS/Letter_Combi.py
@@ -0,0 +1,32 @@
+# You are given a string digits made up of digits from 2 through 9 inclusive.
+
+# Each digit (not including 1) is mapped to a set of characters as shown below:
+
+# A digit could represent any one of the characters it maps to.
+
+# Return all possible letter combinations that digits could represent. You may return the answer in any order.
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+        
+        def dfs(i, curstr):
+            if len(curstr) == len(digits):
+                res.append(curstr)
+                return
+            
+            for c in digitToChar[digits[i]]:
+                dfs(i + 1, curstr + c)
+        if digits:
+            dfs(0, "")
+        return res
