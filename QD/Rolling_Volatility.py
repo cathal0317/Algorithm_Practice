5d2ae2dcff74b12ab0a77e5987cd2acd1c4af76e
Cathal0317 <hlzh0610@gmail.com>
2026-05-04 12:11:48 +0100

    4/5

--- QD/Rolling_Volatility.py
+++ QD/Rolling_Volatility.py
@@ -11,15 +11,32 @@ def Rolling_Volatility(prices: list[int], window: int)->list[float]:
         returns.append(r)
     
     res = []
-    for start in range(len(returns)-window +1):
-        chunk = returns[start:start+window]
+    # for start in range(len(returns)-window +1):
+    #     chunk = returns[start:start+window]
 
-        mean = sum(chunk) / window
-        var = sum((x-mean)**2 for x in chunk) / window
-        std = math.sqrt(var)
+    #     mean = sum(chunk) / window
+    #     var = sum((x-mean)**2 for x in chunk) / window
+    #     std = math.sqrt(var)
 
-        res.append(std)
+    #     res.append(std)
 
+    # return res
+    
+    for start in range(len(returns)-window+1):
+        mean = sum_x/window
+        var = (sum_x2/window) - mean ** 2
+        
+        res.append(math.sqrt(var))
+
+        if start + window < len(returns):
+            old = returns[start]
+            new = returns[start+window]
+
+            sum_x -= old
+            sum_x += new
+
+            sum_x2 -= old * old
+            sum_x2 += new *new
     return res
 
 print(Rolling_Volatility(prices, 3))
